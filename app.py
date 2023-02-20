bbcefe260e8cbbabe937f54911c29e42900f048e
Stephen Hadden <shadden@gmail.com>
2023-02-20 18:29:45 +0000

    added api route

--- app.py
+++ app.py
@@ -1,10 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bengaluru, India',
+    'salary': 'Rs. 10,00,000'
+  },
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Delhi, India'
+  },
+  {
+    'id': 3,
+    'title': 'Frontend engineer',
+    'location': 'Remote',
+    'salary': 'Rs. 12,00,000'
+  },
+  {
+    'id': 4,
+    'title': 'Backend engineer',
+    'location': 'San Fransisco, USA',
+    'salary': '$120,000'
+  }
+]
+
 @app.route("/")
 def hello_world():
-  return render_template("home.html")
+  return render_template("home.html", jobs=JOBS, company_name="Logcabin")
+
+@app.route("/api/jobs") #this is to create a different path with just the data needed - no HTML formating.
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
